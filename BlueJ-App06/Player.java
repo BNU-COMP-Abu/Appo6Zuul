e6e0a8c982df1d8c59ffc05449a36dbe1e28bb6b
AbuAhmed1 <71983256+AbuAhmed1@users.noreply.github.com>
2021-01-09 15:00:38 +0000

    added status

--- BlueJ-App06/Player.java
+++ BlueJ-App06/Player.java
@@ -11,12 +11,36 @@ public class Player
     private int score;
     private int health; 
     private ArrayList <Items> items; 
+    private int moves;
     
     public Player(String name)
     {
+        this.name = name; 
         score = 0;
         health = 100;
         items = new ArrayList <Items> ();
       
     }
+    
+    public void showStatus()
+    {
+        System.out.println("Player Status for " + name);
+        System.out.println("health =  " + health);
+        System.out.println("score =  " + score );
+        System.out.println("move number = " + moves );
+        System.out.println("");
+        
+    }
+    
+    public int getMoves()
+    {
+        return moves;
+    }
+    
+    public void decreaseHealth()
+    {
+        health --;
+        moves ++;
+    }
+    
 }
