f2808b433135cd40ca70f6f5f5f71b32a5e74be8
AbuAhmed1 <71983256+AbuAhmed1@users.noreply.github.com>
2021-01-09 14:34:46 +0000

    Added two Rooms

--- BlueJ-App06/Map.java
+++ BlueJ-App06/Map.java
@@ -7,6 +7,7 @@
  */
 public class Map
 {
+    Room gym, outside, theater, pub, lab, office, fireExit;
     private Room startRoom;
     
     public Map()
@@ -19,19 +20,27 @@ public class Map
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
-      
+        
         // create the rooms
         outside = new Room("outside the main entrance of the university");
         theater = new Room("in a lecture theater");
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
+        gym = new Room("in the gym there is a boxing ring");
+        fireExit = new Room ("your at the back door of the gym and there is a drunkie");
         
         // initialise room exits
         outside.setExit("east", theater);
         outside.setExit("south", lab);
         outside.setExit("west", pub);
+        outside.setExit("north", gym);
+        
+        
+        gym.setExit("south", outside);
+        gym.setExit("fireExit", fireExit);
+        
+        fireExit.setExit("inside", gym);
 
         theater.setExit("west", outside);
 
